b6eeb3a46d17dfd917e716da9c59452cca01d745
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2021-09-21 21:32:02 -0300

    which-key, counsel, ivy-rich, doom-modeline and doom-themes

--- init.el
+++ init.el
@@ -1,9 +1,17 @@
+
 (scroll-bar-mode -1) ; scroll bar disabled
 (tool-bar-mode -1)   ; tool bar disabled
 (menu-bar-mode -1)   ; menu bar disabled
 
-;; Load editor theme
-(load-theme 'wombat)
+;; Line numbers enabled
+(column-number-mode)
+(global-display-line-numbers-mode t)
+;; Disable line numbers for the following modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; Initialize package sources
 (require 'package)
@@ -27,9 +35,72 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; doom-themes (https://github.com/hlissner/emacs-doom-themes)
+;; A theme megapack for GNU Emacs, inspired by community favorites.
+(use-package doom-themes
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t))
+
+;; doom-modeline (https://github.com/seagle0128/doom-modeline)
+;; A fancy and fast mode-line inspired by minimalism design.  
+(use-package doom-modeline
+  :init (doom-modeline-mode 1))
+
 ;; ivy (https://github.com/abo-abo/swiper)
 ;; A generic completion frontend for emacs
 (use-package ivy
   :diminish
   :config
   (ivy-mode 1))
+
+;; counsel (https://github.com/abo-abo/swiper)
+;; A collection of ivy-enhanced versions of common emacs commands.
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)))
+
+;; ivy-rich (https://github.com/Yevgnen/ivy-rich)
+;; Rich transformers for commands from ivy and counsel
+(use-package ivy-rich
+  :config
+  (ivy-rich-mode 1))
+
+;; which-key (https://github.com/justbur/emacs-which-key)
+;; displays the key bindings following your currently entered incomplete
+;; command (a prefix) in a popup
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  ;; Allow C-h to trigger which-key before it is done automatically
+  (setq which-key-show-early-on-C-h t)
+  ;; make sure which-key doesn't show normally but refreshes quickly after it is
+  ;; triggered.
+  (setq which-key-idle-delay 10000)
+  (setq which-key-idle-secondary-delay 0.05))
+
+;; rainbow-delimiters (https://github.com/Fanael/rainbow-delimiters)
+;; Highlights delimiters such as parentheses, brackets or braces
+;; according to their depth.
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(global-display-line-numbers-mode t)
+ '(package-selected-packages
+   '(doom-themes doom-modeline counsel ivy-rich which-key rainbow-delimiters use-package ivy)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
